329815717582fdc5862b0239c0707d67d98aa1f7
Bhavya Sree Pyla <151440469+bhavyasreepyla@users.noreply.github.com>
2026-07-08 17:11:35 -0400

    0.4.1: add GitHub project URLs to package metadata

--- pyla/__init__.py
+++ pyla/__init__.py
@@ -12,7 +12,7 @@ from .evaluator import evaluate
 from .environment import Environment
 from .errors import ParseError, PylaRuntimeError
 
-__version__ = "0.4.0"
+__version__ = "0.4.1"
 
 
 def parse(source, slang=False):
